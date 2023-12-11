bf2eca742c0a5f75f14e1713cc48a3ea30a3c1ae
Ricardo Bosso Sepulveda <ricbos782@contacta.net>
2023-12-11 18:01:31 -0500

    cambios en los scripts

--- php/conexion_chart.php
+++ php/conexion_chart.php
@@ -1,30 +1,23 @@
 <?php
 
-<<<<<<< HEAD
 $conex = mysqli_connect("localhost", "root", "", "años");
-=======
 $conex = mysqli_connect("localhost", "root", "", "año_mes"); // Cambié "año_mes" a "ano_mes" en la conexión
->>>>>>> aa4e660cee97dadd13d34620961d1e6450d13510
 
 if (!$conex) {
     die("No se pudo conectar: " . mysqli_connect_error());
 }
 
-<<<<<<< HEAD
 $sql = "SELECT * FROM suma_años";
 
 // Ejecutar la consulta SQL
-=======
 $sql = "SELECT año, valor_año, valor_mes FROM valores"; // Cambié la consulta SQL para seleccionar las columnas adecuadas
 
->>>>>>> aa4e660cee97dadd13d34620961d1e6450d13510
 $resultado = mysqli_query($conex, $sql);
 
 if (!$resultado) {
     die("Error en la consulta: " . mysqli_error($conex));
 }
 
-<<<<<<< HEAD
 while ($fila = mysqli_fetch_assoc($resultado)) {
     // Aquí puedes acceder a los datos de cada fila
     // Por ejemplo, para imprimir el contenido de una columna llamada "campo_nombre":
@@ -45,7 +38,6 @@ mysqli_free_result($resultado);
 mysqli_close($conex);
 
 
-=======
 // Crear un array asociativo para almacenar los datos
 $data = array();
 
@@ -61,5 +53,4 @@ echo $json_data; // Imprimir el JSON resultante
 mysqli_free_result($resultado);
 mysqli_close($conex);
 
->>>>>>> aa4e660cee97dadd13d34620961d1e6450d13510
 ?>

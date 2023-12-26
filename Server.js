e585e0c240ff5352d7dab48964762b1e85e36cd9
Ricardo Bosso Sepulveda <ricbos782@contacta.net>
2023-12-26 18:00:03 -0500

    endpoints y ajax

--- Server.js
+++ Server.js
@@ -1,80 +1,119 @@
 const express = require('express');
 const mysql = require('mysql');
-const app = express();
+const cors = require('cors'); // Importar el paquete 'cors'
 const port = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.get('/styles/style.css', (req, res) => {
+//   res.setHeader('Content-Type', 'text/css');
+//   res.sendFile(__dirname + '/styles/style.css');
+// });
+
+const dbCampañas = mysql.createConnection({
+  host: '',
+  port: '',
+  user: '',
+  password: '',
+  database: ''
+});
 
 // Configuración de la base de datos
 const db = mysql.createConnection({
-    host: '72.167.77.8',
-    port: 3306,
-    user: 'IT_USER',
-    password: '{Nd8=[So7Uk3',
-    database: 'DATA_NACIONAL'
-});
-
-// Endpoint 5 registros
-app.get('/5-registros', (req, res) => {
-    const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 5'; // Reemplaza nombre_de_tabla por tu tabla real
-    db.query(query, (err, results) => {
-      if (err) {
-        console.error('Error al ejecutar la consulta: ' + err.message);
-        res.status(500).send('Error al obtener los registros');
-      } else {
-        console.log('Registros obtenidos:');
-        console.log(results); // Imprime los registros en la consola del servidor
-        res.status(200).send('Registros obtenidos'); // Envía una respuesta al cliente
-      }
-    });
-});
-
-// Endpoint 10 registros
-app.get('/10-registros', (req, res) => {
-    const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 10'; // Reemplaza nombre_de_tabla por tu tabla real
-    db.query(query, (err, results) => {
-      if (err) {
-        console.error('Error al ejecutar la consulta: ' + err.message);
-        res.status(500).send('Error al obtener los registros');
-      } else {
-        console.log('Registros obtenidos:');
-        console.log(results); // Imprime los registros en la consola del servidor
-        res.status(200).send('Registros obtenidos'); // Envía una respuesta al cliente
-      }
-    });
-});
-
-// Endpoint 25 registros
-app.get('/25-registros', (req, res) => {
-    const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 25'; // Reemplaza nombre_de_tabla por tu tabla real
-    db.query(query, (err, results) => {
-      if (err) {
-        console.error('Error al ejecutar la consulta: ' + err.message);
-        res.status(500).send('Error al obtener los registros');
-      } else {
-        console.log('Registros obtenidos:');
-        console.log(results); // Imprime los registros en la consola del servidor
-        res.status(200).send('Registros obtenidos'); // Envía una respuesta al cliente
-      }
-    });
+  host: '72.167.77.8',
+  port: 3306,
+  user: 'IT_USER',
+  password: '{Nd8=[So7Uk3',
+  database: 'DATA_NACIONAL'
 });
 
 // Conectar a la base de datos
 db.connect(err => {
-    if (err) {
-        console.error('Error al conectar a la base de datos:', err);
-        return;
-    }
-    console.log('Conectado a la base de datos MySQL');
+  if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      return;
+  }
+  console.log('Conectado a la base de datos MySQL');
 });
 
+app.get('/5-registros', (req, res) => {
+  const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 5'; 
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta: ' + err.message);
+      res.status(500).json({ error: 'Error al obtener los registros' });
+    } else {
+      console.log('Registros obtenidos:');
+      console.log(results);
 
-app.get('/obtener-datos', (req, res) => {
-    const cantidad = req.query.cantidad; // Obtiene la cantidad desde la solicitud GET.
-    
-    // Realiza la lógica para obtener los datos basados en la cantidad.
-    // Luego, envía los datos como respuesta.
-    const datos = obtenerDatosDesdeLaBaseDeDatos(cantidad); // Implementa esta función según tu necesidad.
+      // Renderizar la vista y enviarla como respuesta
+      res.render('5-registros', { registros: results }); // '5-registros' es el nombre de la vista
+    }
+  });
+});
 
-    res.json(datos);
+// Endpoint 10 registros
+app.get('/10-registros', (req, res) => {
+  const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 10'; 
+
+  db.query(query, (err, results) => {
+      if (err) {
+          console.error('Error al ejecutar la consulta: ' + err.message);
+          res.status(500).json({ error: 'Error al obtener los registros' }); // Enviar un objeto JSON de error
+      } else {
+          console.log('Registros obtenidos:');
+          console.log(results); // Imprime los registros en la consola del servidor
+          res.status(200).json(results); // Enviar los resultados como objeto JSON
+      }
+  });
+});
+
+// Endpoint 25 registros
+app.get('/25-registros', (req, res) => {
+  const query = 'SELECT * FROM CGN_LLAMADAS_INBOUND LIMIT 25'; 
+
+  db.query(query, (err, results) => {
+      if (err) {
+          console.error('Error al ejecutar la consulta: ' + err.message);
+          res.status(500).json({ error: 'Error al obtener los registros' }); // Enviar un objeto JSON de error
+      } else {
+          console.log('Registros obtenidos:');
+          console.log(results); // Imprime los registros en la consola del servidor
+          res.status(200).json(results); // Enviar los resultados como objeto JSON
+      }
+  });
+});
+
+app.get('/nacionales', (req, res) => {
+  res.sendFile(__dirname + '/Nacional.html');
+});
+
+app.get('/internacionales', (req, res) => {
+  res.sendFile(__dirname + '/Internacional.html');
+});
+
+app.post('/actualizar-message-campaign', (req, res) => {
+  const nuevoContenido = req.body.nuevoContenido; // Obtén el nuevo contenido desde la solicitud POST
+
+  // Actualiza el contenido del contenedor message-campaign con el nuevoContenido
+  // Esto puede implicar guardar el nuevoContenido en la base de datos o realizar alguna otra operación según tus necesidades.
+
+  // Envía una respuesta de éxito
+  res.status(200).json({ mensaje: 'Contenido actualizado con éxito' });
+});
+
+app.post('/mostrar-y-actualizar-message-delete', (req, res) => {
+  const nuevoContenido = req.body.nuevoContenido; // Obtén el nuevo contenido desde la solicitud POST
+
+  // Puedes realizar alguna lógica aquí para actualizar el contenido del contenedor
+  // Esto podría incluir guardar el nuevoContenido en la base de datos o realizar otra operación según tus necesidades.
+
+  // Envía una respuesta de éxito
+  res.status(200).json({ mensaje: 'Contenido actualizado con éxito' });
 });
 
 app.listen(port, () => {
--- main.js
+++ main.js
@@ -1,367 +1,112 @@
-// Eventos (Boton Actualizar todos), (Boton 5 ultimos), (Boton traer 50 registros), (Actualiza ahora), (Icono ajustes), (Boton eliminar), (Boton Nacional e Internacional)
+// Solicitud Ajax 5 regisrors
+document.querySelector('.get-five').addEventListener('click', function () {
+    fetch('/5-registros')
+      .then(response => response.json())
+      .then(data => {
+        // Manejar los datos recibidos, por ejemplo, actualizar la vista
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los últimos 5 registros:', error);
+      });
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.get-five');
-    const tableBody = document.querySelector('#tbody_datos'); // Selección del tbody de la tabla
-
-    button.addEventListener('click', function() {
-        fetch('/midata/last-ten') // Realiza una solicitud GET a la ruta /midata/last-ten
-            .then(response => response.json())
-            .then(data => {
-                // Imprime la respuesta en la consola
-                console.log('Respuesta del servidor:', data);
-
-                // Limpia el contenido actual del tbody
-                tableBody.innerHTML = '';
-
-                // Agrega los datos a la tabla
-                data.forEach(row => {
-                    const newRow = document.createElement('tr');
-                    for (const key in row) {
-                        if (row.hasOwnProperty(key)) {
-                            const cell = document.createElement('td');
-                            cell.textContent = row[key];
-                            newRow.appendChild(cell);
-                        }
-                    }
-                    tableBody.appendChild(newRow);
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
+// Solicitud Ajax 10 regisrors
+document.querySelector('.get-ten').addEventListener('click', function () {
+fetch('/10-registros')  // Debes definir '/10-registros' como una ruta en tu aplicación
+    .then(response => response.json())
+    .then(data => {
+    // Manejar los datos recibidos, por ejemplo, actualizar la vista
+    console.log(data);
+    })
+    .catch(error => {
+    console.error('Error al obtener los últimos 10 registros:', error);
     });
 });
 
-// Eventos (Boton Actualizar todos), (Boton 5 ultimos), (Boton traer 50 registros), (Actualiza ahora), (Icono ajustes), (Boton eliminar), (Boton Nacional e Internacional)
-$(document).ready(function() {
-    // Boton 50 ultimos
-    $('#btn-twentyFive').on('click', function() {
-        const query = "SELECT * FROM CGN_LLAMADAS_INBOUND WHERE STR_TO_DATE(CAL_CALL_START, '%Y-%m-%d %H:%i') BETWEEN STR_TO_DATE('2023-11-1', '%Y-%m-%d') AND STR_TO_DATE('2023-11-2', '%Y-%m-%d') LIMIT 25";
-        updateTable(query);
-    });
-
-    // Boton "5 ultimos"
-    $('.btn-last-five').on('click', function() {
-        // Aquí pones la consulta que deseas enviar al backend para los últimos 10
-        const query = "SELECT * FROM CGN_LLAMADAS_INBOUND ORDER BY DESC LIMIT 5";
-        updateTable(query);
-    });
-
-    // Boton "Actualizar todo"
-    $('#btn-updateAll').on('click', function() {
-        const lastFive = "SELECT * FROM CGN_LLAMADAS_INBOUND ORDER BY DESC LIMIT 5";
-        updateTable(lastFive);
-    });
-
-    // Boton "Actualizar ahora"
-    $('#update-now').on('click', function() {
-        // Hacer una solicitud al nuevo endpoint
-        fetch('http://localhost:3000/update-now', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    });
-
-    // Icono ajustes 
-    $('.btn-setting').on('click', function() {
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/setting-action",
-            success: function (response) {
-                alert('Configuración abierta correctamente'); 
-            },
-            error: function (error) {
-                console.error('Error al abrir configuración:', error);
-            }
-        });
-    });
-
-    // Boton eliminar
-    $('.btn-delete').on('click', function() {
-        const rowId = $(this).closest('tr').text();
-        fetch(`/delete-action/`, {
-            method: 'POST',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al realizar la acción de Delete');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    });
-
-    // Botones Nacionales e Internacional
-    $('#btn-nacionales').on('click', function() {
-        fetch('/nacionales-action', {
-            method: 'POST',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al realizar la acción para Nacionales');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    });
-
-    // Manejar clic en el botón Internacionales
-    $('#btn-internacionales').on('click', function() {
-        fetch('/internacionales-action', {
-            method: 'POST',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al realizar la acción para Internacionales');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    });
+// Solicitud Ajax 25 regisrors
+document.querySelector('#btn-twenty-five').addEventListener('click', function () {
+    fetch('/25-registros')  // Debes definir '/25-registros' como una ruta en tu aplicación
+      .then(response => response.json())
+      .then(data => {
+        // Manejar los datos recibidos, por ejemplo, actualizar la vista
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los últimos 25 registros:', error);
+      });
 });
 
-// Función para actualizar la tabla con datos de la consulta
-function updateTable(query) {
+// Función para cargar contenido de /nacionales
+$("#nacionales").click(function() {
     $.ajax({
-        url: 'http://localhost:3000/query',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({ query: query}),
+        url: "/nacionales",
+        method: "GET",
         success: function(data) {
-            // Vaciar la tabla actual
-            $('#database-table tbody').empty();
-
-            // Iterar sobre los datos y agregar filas a la tabla
-            data.forEach(row => {
-                let html = '<tr>';
-                for (let key in row) {
-                    html += `<td>${row[key]}</td>`;
-                }
-                html += '</tr>';
-                $('#database-table tbody').append(html);
-            });
+            $("#contenido").html(data);
         },
         error: function(error) {
-            console.error('Error:', error);
+            console.error("Error al cargar /nacionales", error);
         }
     });
-}
-
-// Evento clic en fila de la tabla principal
-$('#database-table tbody').on('click', 'tr', function() {
-    const rowData = getRowData($(this));
-    sendRowClickRequest(rowData);
 });
 
-// Evento clic en fila de la tabla principal
-$(document).ready(function() {
-    $('#database-table tbody').on('click', 'tr', function() {
-        const rowData = getRowData($(this));
-        sendTableRelatedRequest(rowData);
-    });
-
-    // Función para enviar la solicitud al servidor cuando se hace clic en una fila
-    function sendTableRelatedRequest(rowData) {
-        $.ajax({
-            url: '/midata/tablas-relacionadas',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ rowData: rowData }),
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-                // Aquí puedes redirigir o realizar otras acciones después del éxito
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    }
-
-    // Función para obtener datos de la fila
-    function getRowData(row) {
-        const rowData = [];
-        row.find('td').each(function() {
-            rowData.push($(this).text());
-        });
-        return rowData;
-    }
-});
-
-
-// Función para enviar la solicitud al servidor cuando se hace clic en una fila
-function sendRowClickRequest(rowData) {
+// Función para cargar contenido de /internacionales
+$("#internacionales").click(function() {
     $.ajax({
-        url: 'http://localhost:3000/midata/row-click',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({ rowData: rowData }),
-        success: function(response) {
-            console.log(response); // Manejar la respuesta del servidor
+        url: "/internacionales",
+        method: "GET",
+        success: function(data) {
+            $("#contenido").html(data);
         },
         error: function(error) {
-            console.error('Error:', error);
+            console.error("Error al cargar /internacionales", error);
         }
     });
-}
+});
 
-// Eventos para los botones (Azules y Naranjas) de la pestaña Login y Register
+// Funcion para agregar campaña 
 $(document).ready(function() {
-    // Evento clic para el botón azul de inicio de sesión
-    $('#btn-sign-in').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/login',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-                // Aquí puedes redirigir o realizar otras acciones después del éxito
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
+    $('#actualizarBoton').on('click', function() {
+      const nuevoContenido = 'Nuevo contenido para la campaña'; // El nuevo contenido que deseas establecer
 
-    // Evento clic para el botón azul de registro
-    $('#btn-sign-up').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/register',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-                // Aquí puedes redirigir o realizar otras acciones después del éxito
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
-
-    // Evento clic para el botón naranja de inicio de sesión
-    $('#btn-login').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/btn-login',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-                // Aquí puedes redirigir o realizar otras acciones después del éxito
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
-
-    // Evento clic para el botón naranja de registro
-    $('#btn-register').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/btn-register',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-                // Aquí puedes redirigir o realizar otras acciones después del éxito
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
+      // Realiza una solicitud POST al nuevo endpoint
+      $.ajax({
+        url: '/actualizar-message-campaign',
+        method: 'POST',
+        data: { nuevoContenido: nuevoContenido },
+        success: function(response) {
+          // Actualiza el contenido del contenedor message-campaign con el nuevoContenido
+          $('#contenidoCampaign').text(nuevoContenido);
+          $('.message-campaign').show(); // Muestra el contenedor si estaba oculto
+          alert(response.mensaje); // Muestra un mensaje de éxito
+        },
+        error: function(error) {
+          alert('Error al actualizar el contenido: ' + error.statusText);
+        }
+      });
     });
 });
 
-// Evento del formulario de ajustes de periodicidad
+// Funcion para eliminar campaña
 $(document).ready(function() {
-    $("#btn-apply").on("click", function() {
-        // Obtén los valores de los campos de entrada con jQuery
-        const days = $("#days").val();
-        const hours = $("#hours").val();
-        const minutes = $("#minutes").val();
+    $('#mostrarActualizarBoton').on('click', function() {
+      const nuevoContenido = 'Nuevo contenido para eliminar campaña'; // El nuevo contenido que deseas establecer
 
-        // Realiza la solicitud POST al endpoint del servidor con jQuery
-        $.ajax({
-            url: '/midata/submit-form',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify({ days, hours, minutes }),
-            success: function(data) {
-                console.log('Respuesta del servidor:', data);
-            },
-            error: function(error) {
-                console.error('Error al enviar la solicitud:', error);
-            }
-        });
+      // Realiza una solicitud POST al nuevo endpoint
+      $.ajax({
+        url: '/mostrar-y-actualizar-message-delete',
+        method: 'POST',
+        data: { nuevoContenido: nuevoContenido },
+        success: function(response) {
+          // Actualiza el contenido del contenedor message-delete con el nuevoContenido
+          $('#contenidoDelete').text(nuevoContenido);
+          $('.message-delete').show(); // Muestra el contenedor si estaba oculto
+          alert(response.mensaje); // Muestra un mensaje de éxito
+        },
+        error: function(error) {
+          alert('Error al mostrar/actualizar el contenido: ' + error.statusText);
+        }
+      });
     });
 });
-
-// Eventos de (BOTON APLICAR), (BOTON DESACTIVAR CAMPAÑA),(IMAGEN DE AJUSTES)
-$(document).ready(function() {
-    // Evento clic para el botón Aplicar
-    $('#btn-apply').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/apply',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-
-                // Puedes realizar otras acciones después del éxito si es necesario
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
-
-    // Evento clic para el botón Desactivar campaña
-    $('#btn-inactive-campaign').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/inactive-campaign',
-            success: function(response) {
-                console.log(response); // Manejar la respuesta del servidor
-
-                // Puedes realizar otras acciones después del éxito si es necesario
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
-
-    // Evento clic para la imagen con ID icon-edit
-    $('#icon-edit').on('click', function() {
-        $.ajax({
-            type: 'POST',
-            url: '/midata/edit-icon',
-            success: function(response) {
-                console.log(response); 
-            },
-            error: function(error) {
-                console.error('Error:', error);
-            }
-        });
-    });
-});
-
